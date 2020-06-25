7d8a4d32e8263f355ebef690266a6122a0e78cb2
Ripudaman Singh <ripu502@gmail.com>
2020-06-26 03:52:36 +0530

    Github Pages deployment

---
+++ precache-manifest.df5e32a7d8d063bfd89d23193c372a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589842edc8d30dde7b3f537a24236f64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0301d24fa570ee51455f",
+    "url": "/static/css/main.426d8c0b.chunk.css"
+  },
+  {
+    "revision": "45844acfba37536f64bd",
+    "url": "/static/js/2.7e6469ef.chunk.js"
+  },
+  {
+    "revision": "9975de65ed488f025f76f90ef49f502f",
+    "url": "/static/js/2.7e6469ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0301d24fa570ee51455f",
+    "url": "/static/js/main.2b4bb1cb.chunk.js"
+  },
+  {
+    "revision": "92efe62ac771f3d04de5",
+    "url": "/static/js/runtime-main.ff4f2b8d.js"
+  }
+]);
